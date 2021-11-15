ee2bef038d3efb7a47d8c8e7c86c9aeb2326879f
dalonsoit98 <daniel.alonso@mail.polimi.it>
2021-11-15 22:05:53 +0100

    Volume Slider Fixed and applied

--- Polimi On Fire/Assets/Scripts/menu/VolumeMenu.cs
+++ Polimi On Fire/Assets/Scripts/menu/VolumeMenu.cs
@@ -1,12 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+
 
 public class VolumeMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider VolumeSlider;
+    private float volume;
+
+    private void Start()
+    {
+       audioMixer.GetFloat("volume",out volume);
+       VolumeSlider.value = volume;
+    }
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -15,4 +27,5 @@ public class VolumeMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Options");
     }
+    
 }
