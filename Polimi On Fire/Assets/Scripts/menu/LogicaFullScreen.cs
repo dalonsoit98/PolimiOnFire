8717ef339195234e379b7cfeca2d93f794ebc025
miguelgh65polimi <miguel2.gonzalez@mail.polimi.it>
2022-01-04 13:49:45 +0100

    bugs in buildings and graphics menu

--- Polimi On Fire/Assets/Scripts/menu/LogicaFullScreen.cs
+++ Polimi On Fire/Assets/Scripts/menu/LogicaFullScreen.cs
@@ -11,8 +11,7 @@ public class LogicaFullScreen : MonoBehaviour
     // Start is called before the first frame update
 
 
-    public TMP_Dropdown resolucionesDropDown;
-    private Resolution[] resoluciones;
+   
     void Start()
     {
         if (Screen.fullScreen)
@@ -23,7 +22,6 @@ public class LogicaFullScreen : MonoBehaviour
         {
             toggle.isOn = false;
         }
-		RevisarResolucion();
         
     }
 
@@ -39,54 +37,7 @@ public class LogicaFullScreen : MonoBehaviour
     }
     
 
-    public void RevisarResolucion()
-    {
-        resoluciones = Screen.resolutions;
-        resolucionesDropDown.ClearOptions();
-        List<string> opciones = new List<string>();
-        int resolucionActual = 0;
-		
-        for (int i = 0; i < resoluciones.Length; i++)
-        {
-			
-			if (resoluciones[i].height!=1080){
- 				if (resoluciones[i].height!=720) {
-					if(resoluciones[i].height!=1440){
-						if(resoluciones[i].height!=2160)continue;}}}
-
-			
-
-
-			
-			
-
-
-			
-
-            string opcion = resoluciones[i].width + " x " + resoluciones[i].height + " @" + resoluciones[i].refreshRate;
-            opciones.Add(opcion);
-
-            if (Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width &&
-                resoluciones[i].height == Screen.currentResolution.height && resoluciones[i].refreshRate== Screen.currentResolution.refreshRate)
-            {
-                resolucionActual = i;
-
-            }
-
-
-			
-		
-            resolucionesDropDown.AddOptions(opciones);
-            resolucionesDropDown.value = resolucionActual;
-            resolucionesDropDown.RefreshShownValue();
-        }
-    }
-
-    public void CambiarResolucion(int indiceResolucion)
-        {
-            Resolution resolucion = resoluciones[indiceResolucion];
-            Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen, resolucion.refreshRate);
-        }
+ 
         
         
     }
